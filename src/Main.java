2b87c5eb65812b79f6ef6033911d4634d0dc75cf
Adjlout Amazigh <amazighadjlout@gmail.com>
2023-11-21 22:29:06 +0100

    First Work Together !

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+import javax.swing.*;
+import java.awt.Color;
+
 public class Main{
-    System.out.print("hello poo");
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setTitle("Star Ship");
+        window.setSize(600, 600);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        JPanel gamePanel = new JPanel();
+        gamePanel.setBackground(Color.BLACK);
+        window.add(gamePanel);
+
+    }
+
 }
