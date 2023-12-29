d37adc9fd429d223e4f9317206a6615ed839df17
Yacine <yy656666@gmail.com>
2023-12-30 00:35:17 +0100

    Refactor Bat class and Enemy class

--- src/gameobjects/Bat.java
+++ src/gameobjects/Bat.java
@@ -1,86 +1,22 @@
 package gameobjects;
 
-import world.Level;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Bat extends Enemy {
-    private int x;
-    private int y;
-    private boolean goingUp = false;
-    private int health = 2;
-    private final int speed = 1;
-    private int damage = 1;
-    private Level level = Level.getInstance();
+public class Bat extends Enemy {  
 
     public Bat(int x, int y) {
         this.x = x;
         this.y = y;
-
+        this.health = 2;
+        this.speed = 1;
+        this.damage = 1;
     }
 
     @Override
-    public int getX() {
-        return x;
-    }
-
-    @Override
-    public int getY() {
-        return y;
-    }
-
-    public void advance() {
-        if (x<0) {
-            x++;
-            return;
-        }
-        
-        if (x + 1 < level.getArena()[0].length && (level.getArena()[y][x + 1] == 1 || level.getArena()[y][x + 1] == 3)) {
-            //go right
-            x += speed;
-            goingUp = false;
-        } else if (y + 1 < level.getArena().length && !goingUp && (level.getArena()[y + 1][x] == 1 || level.getArena()[y + 1][x] == 3)) {
-            //go down
-            y += speed;   
-        } else if (y - 1 >= 0 && (level.getArena()[y - 1][x] == 1 || level.getArena()[y - 1][x] == 3)) {
-            //go up
-            y -= speed;
-            goingUp = true;
-        } else if (x - 1 >= 0 && (level.getArena()[y][x - 1] == 1 || level.getArena()[y][x - 1] == 3)) {
-            //go left
-            x -= speed;
-            goingUp = false;
-        } 
-    }
-
-    @Override
-    public boolean isAtBase() {
-        if (x < 0 ) return false;
-        if (level.getArena()[y][x] == 3) return true;
-        return false;
-    }
-
-    @Override
-    public void takeDamage(int damage) {
-        health -= damage;
-        if (health <= 0) {
-            //delete bat
-        }
-    }
-
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-
     public Image getImage() {
         BufferedImage img = null;
         try {
@@ -92,9 +28,4 @@ public class Bat extends Enemy {
         return scaledImg;
     }
 
-
-    @Override
-    public int getDamage() {
-        return damage;
-    }
 }
--- src/gameobjects/Enemy.java
+++ src/gameobjects/Enemy.java
@@ -1,15 +1,63 @@
 package gameobjects;
+import java.awt.Image;
+
 import world.Level;
 
-import java.awt.*;
-
 public abstract class Enemy extends GameObject {
+    protected int health;
+    protected int speed;
+    protected boolean goingUp;
+    protected int damage;
 
-    public abstract void advance();
-    abstract void takeDamage(int damage);
 
-    public abstract int getHealth();
+    public void advance() {
+        if (x<0) {
+            x++;
+            return;
+        }
+        
+        if (x + 1 < Level.getInstance().getArena()[0].length && (Level.getInstance().getArena()[y][x + 1] == 1 || Level.getInstance().getArena()[y][x + 1] == 3)) {
+            //go right
+            x += speed;
+            goingUp = false;
+        } else if (y + 1 < Level.getInstance().getArena().length && !goingUp && (Level.getInstance().getArena()[y + 1][x] == 1 || Level.getInstance().getArena()[y + 1][x] == 3)) {
+            //go down
+            y += speed;   
+        } else if (y - 1 >= 0 && (Level.getInstance().getArena()[y - 1][x] == 1 || Level.getInstance().getArena()[y - 1][x] == 3)) {
+            //go up
+            y -= speed;
+            goingUp = true;
+        } else if (x - 1 >= 0 && (Level.getInstance().getArena()[y][x - 1] == 1 || Level.getInstance().getArena()[y][x - 1] == 3)) {
+            //go left
+            x -= speed;
+            goingUp = false;
+        } 
+    }
+    
+    public boolean isAtBase() {
+        if (x < 0 ) return false;
+        if (Level.getInstance().getArena()[y][x] == 3) return true;
+        return false;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            //delete bat
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
     public abstract Image getImage();
-    public abstract boolean isAtBase();
-    public abstract int getDamage();
 }
